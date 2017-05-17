e56643339475dd0bd5a05f07ed057602b39cdfdd
Anthony Nichols <hi@anthonynichols.me>
2017-05-17 16:55:19 -0500

    Update store

--- src/store.ts
+++ src/store.ts
@@ -8,10 +8,33 @@
 // Types
 // -----------------------------------------------------------------------------
 
+namespace Store {
+  export interface Constructor {
+    afterSerialization?: () => void;
+    autoload?: boolean;
+    beforeDeserialization?: () => void;
+    filename?: string;
+    onLoad?: () => void;
+    timestamp?: boolean;
+  }
+}
+
 // -----------------------------------------------------------------------------
 // Store
 // -----------------------------------------------------------------------------
 
 export class Store {
+  filename: string;
 
+  constructor(options: Store.Constructor) {
+    let {
+      autoload = false,
+      filename = null,
+      timestamp = false,
+    } = options;
+
+    if (!filename || typeof filename !== 'string' || filename.length === 0) {
+
+    }
+  }
 }
